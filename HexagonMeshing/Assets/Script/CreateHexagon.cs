be5fe4e2a62faf0d9229ba6b074f15dbce87763e
brianboersen <19965@ma-web.nl>
2017-09-27 11:21:26 +0200

    hexagon meshes

---
+++ HexagonMeshing/Assets/Script/CreateHexagon.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateHexagon : MonoBehaviour
+{
+    private Vector3[] newVertices;
+    private int[] newTriangles;
+    private Material defaultMat = new Material(Shader.Find("Diffuse"));
+
+    void Awake()
+    {
+        //creating triangles beforehand, because this 3d hexagon always has the same triangle pattern
+        newTriangles = new int[] {0,1,2,0,2,3,0,3,4,0,4,5,0,5,6,0,6,1};//upper part 0,1,2,0,2,3,0,3,4,0,4,5,0,5,6,0,6,1
+
+        //test
+        //createNewHexagon(1);
+    }
+
+    //creating a 3d hexagon
+    public GameObject createNewHexagon(float size, Material material = null)
+    { 
+        //creating and array of vertesices
+        calcVertices(size);
+
+        //greating hexagon object
+        GameObject hexagon = new GameObject();
+
+        //putting the nessesary components on the hexagon
+        hexagon.AddComponent<MeshFilter>();
+        hexagon.AddComponent<MeshRenderer>();  
+
+        //getting the mesh an renderer of the hexagon
+        Mesh mesh = hexagon.GetComponent<MeshFilter>().mesh;
+        MeshRenderer renderer = hexagon.GetComponent<MeshRenderer>();
+
+        //put material on object
+        if(material == null)
+            renderer.sharedMaterial = defaultMat;
+        else
+            renderer.sharedMaterial = material;
+
+        //set mesh
+        mesh.vertices = newVertices;
+        mesh.triangles = newTriangles;
+
+        //this is for lighting
+        mesh.RecalculateNormals();
+
+        return hexagon;
+    }
+
+    private void calcVertices(float size)
+    {
+        // get b lenght with pythagoras
+        float halfSize = size * 0.5f;
+        float bSize = Mathf.Sqrt((size*size) - (halfSize * halfSize));
+
+        // set the vertices of the hexagon
+        newVertices = new Vector3[] { new Vector3(0, halfSize, 0), new Vector3(0, halfSize, size), new Vector3(bSize, halfSize, halfSize),new Vector3(bSize, halfSize, -halfSize), new Vector3(0, halfSize, -size), new Vector3(-bSize, halfSize, -halfSize), new Vector3(-bSize, halfSize, halfSize), new Vector3(0, -halfSize, 0), new Vector3(0, -halfSize, size), new Vector3(bSize, -halfSize, halfSize), new Vector3(bSize, -halfSize, -halfSize), new Vector3(0, -halfSize, -size), new Vector3(-bSize, -halfSize, -halfSize), new Vector3(-bSize, -halfSize, halfSize) };
+    }
+}
---
+++ HexagonMeshing/Assets/Script/HexaGrid.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexaGrid : MonoBehaviour
+{
+    [SerializeField]
+    private float hexagonSize = 1;
+
+    [SerializeField]
+    private int gridXSize = 11;
+
+    [SerializeField]
+    private int gridYSize = 3;
+
+    public GameObject[,] hexagrid;
+
+    private CreateHexagon createHex;
+
+    void Start ()
+    {
+        hexagrid = new GameObject[gridXSize, gridYSize];
+        createHex = GetComponent<CreateHexagon>();
+        createGrid();
+    }
+
+    //zet hexagons in script
+    public void createGrid()
+    {
+        //GameObject hexagon = createHex.createNewHexagon(hexagonSize);
+        int gritXLenght = hexagrid.GetLength(0);
+
+        float gridRowStep;
+
+        //side step calculate
+        float upStepSize = Mathf.Sqrt((hexagonSize * hexagonSize) - ((hexagonSize * 0.5f) * (hexagonSize * 0.5f)));
+
+        for (int x = 0; x < gritXLenght; x++)//gritXLenght
+        {
+            if (x % 2 == 0)
+            {
+                gridRowStep = 0;
+            }
+            else
+            {
+                gridRowStep = hexagonSize * 1.5f;
+            }
+
+            for (int y = 0; y < hexagrid.GetLength(1); y++)
+            {
+                //create new hexagon
+                GameObject newHexagon = createHex.createNewHexagon(hexagonSize); //Instantiate(hexagon, Vector3.zero, Quaternion.identity);
+
+                newHexagon.name = "hexagon " + ((x * gritXLenght) + y);
+
+                //put hexagon in the correct spot
+                newHexagon.transform.localPosition = new Vector3(x * upStepSize,0, (y * (hexagonSize * 3)) + gridRowStep);
+
+                //add hexagen to grid array
+                hexagrid[x, y] = newHexagon;
+            }
+        }
+    }
+}
